6f063de22777c61dc6c1a2d0551a05442ffc6227
Eran Kampf <eran@ekampf.com>
2016-01-11 00:04:03 -0800

    bootstrap

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+require("bootstrap-webpack");
 require('./styles/main.scss');
 
 ReactDOM.render(
-  <div>
-      <h1>Hello, world!</h1>
+  <div className="container">
+      <h1>Hello, world!!</h1>
   </div>,
   document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,17 @@ module.exports = {
     },
     module: {
         loaders: [
+            // **IMPORTANT** This is needed so that each bootstrap js file required by
+            // bootstrap-webpack has access to the jQuery object
+            { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+            // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+            // loads bootstrap's css.
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" },
+
             {
                 test: /\.html$/,
                 loader: 'file?name=[name].[ext]'
@@ -48,7 +59,7 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
         new webpack.DefinePlugin({
             'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
-        })
+        }),
     ],
     devServer: {
         contentBase: './app',
